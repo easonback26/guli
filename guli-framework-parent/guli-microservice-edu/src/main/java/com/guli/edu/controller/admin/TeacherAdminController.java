0a07b3273cac0920524ab91133373f33ea68738d
Shadow Song <easonback@gmail.com>
2020-01-12 22:34:26 -0500

    Done No.4
    第四教程做完

--- guli-framework-parent/guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
+++ guli-framework-parent/guli-microservice-edu/src/main/java/com/guli/edu/controller/admin/TeacherAdminController.java
@@ -1,15 +1,21 @@
 
 package com.guli.edu.controller.admin;
 
+//import com.baomidou.mybatisplus.extension.api.R;
+
+import com.guli.common.vo.R;
 import com.guli.edu.entity.Teacher;
 import com.guli.edu.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@Api("讲师管理")
 @CrossOrigin
 @RequestMapping("/admin/edu/teacher")
 
@@ -19,16 +25,21 @@ public class TeacherAdminController {
     private TeacherService teacherService;
 
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping
-    public List<Teacher> list(){
-
-        return teacherService.list(null);
-
+    public R list(){
+        List<Teacher> list = teacherService.list(null);
+        return R.ok().data("items", list);
     }
 
-    @DeleteMapping("{id}")
-    public boolean removeById(@PathVariable String id){
-        return teacherService.removeById(id);
-    }
+
+//    @ApiOperation(value = "根据ID删除讲师")
+//    @DeleteMapping("{id}")
+//    public boolean removeById(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id){
+//
+//        return teacherService.removeById(id);
+//    }
+
+
 
 }
